5c7f11befb4645ec0f9d9a0104ff704a6fcca720
Denki05 <106417282+Denki05@users.noreply.github.com>
2025-04-28 11:24:08 +0700

    Adjust table layout in product listing for improved readability

--- resources/views/master/produk/index.blade.php
+++ resources/views/master/produk/index.blade.php
@@ -10,7 +10,7 @@
             <table class="min-w-full table-auto">
                 <thead class="bg-gray-100">
                     <tr>
-                        <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider whitespace-nowrap">Code - Name</th>
+                        <th class="px-2 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider whitespace-nowrap">Code - Name</th>
                         <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider whitespace-nowrap">Brand</th>
                         <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider whitespace-nowrap">Category</th>
                         <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider whitespace-nowrap">Status</th>
@@ -19,7 +19,7 @@
                 <tbody class="divide-y divide-gray-200 text-sm">
                     @foreach ($products as $product)
                     <tr>
-                        <td class="px-6 py-4 whitespace-nowrap">{{ $product['code'] ?? '-' }} - {{ $product['name'] ?? '-' }}</td>
+                        <td class="px-2 py-4 whitespace-nowrap">{{ $product['code'] ?? '-' }}<br><strong>{{ $product['name'] ?? '-' }}</strong></td>
                         <td class="px-6 py-4 whitespace-nowrap">{{ $product['brand_name'] }}</td>
                         <td class="px-6 py-4 whitespace-nowrap">{{ $product['category_name'] ?? '-' }}</td> <!-- Menampilkan nama kategori -->
                         <td class="px-6 py-4 whitespace-nowrap">
